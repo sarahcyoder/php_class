eac5e4026a83c0fda4234d83c382ac6004cf1884
Sarah <sarahcyoder@gmail.com>
2018-01-05 09:19:04 -0500

    Delete connectvars.php

--- sarahyoder-L5/connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // database connection constants
-  define('HOST', 'gator3258.hostgator.com');
-  define('DBNAME', 'syoder_phpclass_v2');
-  define('USER', 'syoder_webUser5');
-  define('PW', '3?+5._=o5T7a');
-?>
